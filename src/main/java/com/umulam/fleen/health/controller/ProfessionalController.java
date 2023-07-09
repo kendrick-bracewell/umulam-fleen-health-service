4b03866ef39e581b93a50ea8260b7edde974b9b2
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-09 14:31:18 +0100

    create controller and service for professional

--- src/main/java/com/umulam/fleen/health/controller/ProfessionalController.java
+++ src/main/java/com/umulam/fleen/health/controller/ProfessionalController.java
@@ -1,4 +1,13 @@
 package com.umulam.fleen.health.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "professional")
 public class ProfessionalController {
+
+
 }
---
+++ src/main/java/com/umulam/fleen/health/service/impl/ProfessionalServiceImpl.java
@@ -0,0 +1,4 @@
+package com.umulam.fleen.health.service.impl;
+
+public class ProfessionalServiceImpl {
+}
