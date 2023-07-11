a75fdc4690ca4147d0a6550bc665c3b014056f2c
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-11 22:00:36 +0100

    remove noargsconstructor annotation

--- src/main/java/com/umulam/fleen/health/model/response/member/UpdateMemberDetailsResponse.java
+++ src/main/java/com/umulam/fleen/health/model/response/member/UpdateMemberDetailsResponse.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
@@ -16,7 +15,6 @@ import static com.umulam.fleen.health.util.DateFormatUtil.DATE_TIME_FORMAT;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdateMemberDetailsResponse {
 
