f8625056cfa0e3ecf54dcd33193e846da62b9ee4
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-17 21:29:03 +0100

    fix validation issues

--- src/main/java/com/umulam/fleen/health/validator/MaxAvailabilityTimeInADay.java
+++ src/main/java/com/umulam/fleen/health/validator/MaxAvailabilityTimeInADay.java
@@ -1,13 +1,14 @@
 package com.umulam.fleen.health.validator;
 
 import com.umulam.fleen.health.validator.impl.IsNumberValidator;
+import com.umulam.fleen.health.validator.impl.MaxAvailabilityTimeInADayValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = IsNumberValidator.class)
+@Constraint(validatedBy = MaxAvailabilityTimeInADayValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MaxAvailabilityTimeInADay {
