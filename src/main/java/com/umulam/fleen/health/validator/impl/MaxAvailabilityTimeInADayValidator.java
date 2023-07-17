daaac1e3f5990574a05fea2b77c22eb4e625c8b4
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-17 23:54:59 +0100

    handle exception for invalid day of the week

--- src/main/java/com/umulam/fleen/health/validator/impl/MaxAvailabilityTimeInADayValidator.java
+++ src/main/java/com/umulam/fleen/health/validator/impl/MaxAvailabilityTimeInADayValidator.java
@@ -1,31 +1,41 @@
 package com.umulam.fleen.health.validator.impl;
 
 import com.umulam.fleen.health.constant.professional.AvailabilityDayOfTheWeek;
-import com.umulam.fleen.health.model.dto.professional.UpdateProfessionalAvailabilityDto;
 import com.umulam.fleen.health.validator.MaxAvailabilityTimeInADay;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.umulam.fleen.health.model.dto.professional.UpdateProfessionalAvailabilityDto.AvailabilityPeriod;
 
 @Slf4j
-public class MaxAvailabilityTimeInADayValidator implements ConstraintValidator<MaxAvailabilityTimeInADay, List<UpdateProfessionalAvailabilityDto.AvailabilityPeriod>> {
+public class MaxAvailabilityTimeInADayValidator implements ConstraintValidator<MaxAvailabilityTimeInADay, List<AvailabilityPeriod>> {
 
   @Override
   public void initialize(MaxAvailabilityTimeInADay constraintAnnotation) { }
 
   @Override
-  public boolean isValid(List<UpdateProfessionalAvailabilityDto.AvailabilityPeriod> availabilityPeriods, ConstraintValidatorContext constraintValidatorContext) {
+  public boolean isValid(List<AvailabilityPeriod> availabilityPeriods, ConstraintValidatorContext constraintValidatorContext) {
     if (!Objects.isNull(availabilityPeriods) && !availabilityPeriods.isEmpty()) {
       Map<AvailabilityDayOfTheWeek, List<String>> availabilityTimes = new HashMap<>();
-      availabilityPeriods
+      availabilityPeriods = availabilityPeriods
         .stream()
         .filter(Objects::nonNull)
-        .forEach(period -> {
+        .collect(Collectors.toList());
+
+      for (AvailabilityPeriod period : availabilityPeriods) {
+        try {
           AvailabilityDayOfTheWeek dayOfTheWeek = AvailabilityDayOfTheWeek.valueOf(period.getDayOfTheWeek());
           addToMap(availabilityTimes, dayOfTheWeek, null);
-        });
+        } catch (IllegalArgumentException ex) {
+          log.error(ex.getMessage(), ex);
+          return false;
+        }
+      }
+
       for (var entry: availabilityTimes.entrySet()) {
         int TOTAL_NUMBER_OF_WORKING_HOURS = 9;
         if (entry.getValue().size() > TOTAL_NUMBER_OF_WORKING_HOURS) {
