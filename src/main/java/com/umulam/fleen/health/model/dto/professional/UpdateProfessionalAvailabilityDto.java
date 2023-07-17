767abecf46b0f3ba523db0a79dd710c1fab7b759
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-17 21:26:43 +0100

    clean and remove unused imports

--- src/main/java/com/umulam/fleen/health/model/dto/professional/UpdateProfessionalAvailabilityDto.java
+++ src/main/java/com/umulam/fleen/health/model/dto/professional/UpdateProfessionalAvailabilityDto.java
@@ -3,18 +3,13 @@ package com.umulam.fleen.health.model.dto.professional;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.umulam.fleen.health.constant.professional.AvailabilityDayOfTheWeek;
 import com.umulam.fleen.health.validator.*;
-import com.umulam.fleen.health.validator.impl.TimeValidValidator;
 import lombok.*;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalTime;
 import java.util.List;
-
-import static org.springframework.format.annotation.DateTimeFormat.ISO.TIME;
 
 @Builder
 @Getter
