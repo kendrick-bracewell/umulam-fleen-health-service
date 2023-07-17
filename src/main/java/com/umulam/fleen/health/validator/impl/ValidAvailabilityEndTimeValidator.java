6eee1b8739d43ee32573f23e72f3e4971f3f23fa
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-17 21:31:46 +0100

    fix start time validator issues

--- src/main/java/com/umulam/fleen/health/validator/impl/ValidAvailabilityEndTimeValidator.java
+++ src/main/java/com/umulam/fleen/health/validator/impl/ValidAvailabilityEndTimeValidator.java
@@ -6,10 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalTime;
-import java.util.Objects;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import static com.umulam.fleen.health.util.DateFormatUtil.TIME;
+import static java.util.Objects.nonNull;
 
 @Slf4j
-public class ValidAvailabilityEndTimeValidator implements ConstraintValidator<ValidAvailabilityEndTime, LocalTime> {
+public class ValidAvailabilityEndTimeValidator implements ConstraintValidator<ValidAvailabilityEndTime, String> {
 
   LocalTime workingHoursStart = LocalTime.of(9, 0);
   LocalTime workingHoursEnd = LocalTime.of(18, 0);
@@ -18,7 +22,16 @@ public class ValidAvailabilityEndTimeValidator implements ConstraintValidator<Va
   public void initialize(ValidAvailabilityEndTime constraintAnnotation) {}
 
   @Override
-  public boolean isValid(LocalTime startTime, ConstraintValidatorContext constraintValidatorContext) {
-    return !Objects.isNull(startTime) && !startTime.isBefore(workingHoursStart) && !startTime.isAfter(workingHoursEnd);
+  public boolean isValid(String time, ConstraintValidatorContext constraintValidatorContext) {
+    if (nonNull(time)) {
+      try {
+        final DateTimeFormatter dtf = DateTimeFormatter.ofPattern(TIME);
+        LocalTime startTime = LocalTime.parse(time, dtf);
+        return startTime.isBefore(workingHoursStart) && !startTime.isAfter(workingHoursEnd);
+      } catch (DateTimeParseException ex) {
+        log.error(ex.getMessage(), ex);
+      }
+    }
+    return false;
   }
 }
