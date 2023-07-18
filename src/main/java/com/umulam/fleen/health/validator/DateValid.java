9b49b485ef598f93dee91e9c3d41599cf5014261
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-18 12:03:13 +0100

    remove unused import and fix validator

--- src/main/java/com/umulam/fleen/health/validator/DateValid.java
+++ src/main/java/com/umulam/fleen/health/validator/DateValid.java
@@ -1,13 +1,13 @@
 package com.umulam.fleen.health.validator;
 
-import com.umulam.fleen.health.validator.impl.DateOfBirthValidator;
+import com.umulam.fleen.health.validator.impl.DateValidValidator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = DateOfBirthValidator.class)
+@Constraint(validatedBy = DateValidValidator.class)
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DateValid {
