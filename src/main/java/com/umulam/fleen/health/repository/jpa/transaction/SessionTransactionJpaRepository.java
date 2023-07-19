53ab0679e8e63e8be518b5493aa2befab23c3f5f
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-19 17:17:23 +0100

    complete and update transaction details

---
+++ src/main/java/com/umulam/fleen/health/repository/jpa/transaction/SessionTransactionJpaRepository.java
@@ -0,0 +1,2 @@
+package com.umulam.fleen.health.repository.jpa.transaction;public class SessionTransactionJpaRepository {
+}
--- src/main/java/com/umulam/fleen/health/repository/jpa/transaction/TransactionJpaRepository.java
+++ src/main/java/com/umulam/fleen/health/repository/jpa/transaction/TransactionJpaRepository.java
@@ -3,6 +3,10 @@ package com.umulam.fleen.health.repository.jpa;
 import com.umulam.fleen.health.model.domain.transaction.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransactionJpaRepository extends JpaRepository<Transaction, Integer> {
+
+  Optional<Transaction> findByReference(String reference);
 
 }
