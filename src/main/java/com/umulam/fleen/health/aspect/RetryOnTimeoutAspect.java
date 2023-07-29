26d6f21428079bb438e06c051f679cf24658266f
Yusuf Alamu Musa <volunux@gmail.com>
2023-07-29 15:04:41 +0100

    refactor method for retrying request

--- src/main/java/com/umulam/fleen/health/aspect/RetryOnTimeoutAspect.java
+++ src/main/java/com/umulam/fleen/health/aspect/RetryOnTimeoutAspect.java
@@ -1,21 +1,13 @@
 package com.umulam.fleen.health.aspect;
 
-import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-@Slf4j
 @Aspect
 @Component
 public class RetryOnTimeoutAspect {
-
-  private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
   @Around("@annotation(retryOnTimeout)")
   public Object retryOnTimeout(ProceedingJoinPoint joinPoint, RetryOnTimeout retryOnTimeout) throws Throwable {
@@ -27,25 +19,15 @@ public class RetryOnTimeoutAspect {
       try {
         return joinPoint.proceed();
       } catch (Throwable throwable) {
-        attempts++;
         if (attempts >= maxAttempts) {
           throw throwable;
         }
 
-        scheduleRetry(joinPoint, retryOnTimeout, attempts, timeoutMillis);
+        attempts++;
+        Thread.sleep(timeoutMillis);
       }
     } while (attempts < maxAttempts);
 
     return null;
-  }
-
-  private void scheduleRetry(ProceedingJoinPoint joinPoint, RetryOnTimeout retryOnTimeout, int attempt, long timeoutMillis) {
-    executorService.schedule(() -> {
-      try {
-        joinPoint.proceed();
-      } catch (Throwable throwable) {
-        log.error(throwable.getMessage());
-      }
-    }, timeoutMillis, TimeUnit.MILLISECONDS);
   }
 }
