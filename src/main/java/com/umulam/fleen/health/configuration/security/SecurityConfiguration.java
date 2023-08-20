e5c17b4a5113b0da0082a51ba05e307a83308064
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-20 21:04:01 +0100

    remove corsConfigurer and add simplecorsfilter

--- src/main/java/com/umulam/fleen/health/configuration/security/SecurityConfiguration.java
+++ src/main/java/com/umulam/fleen/health/configuration/security/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package com.umulam.fleen.health.configuration.security;
 import com.umulam.fleen.health.configuration.security.provider.CustomAuthenticationProvider;
 import com.umulam.fleen.health.filter.JwtAuthenticationFilter;
 import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -16,8 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -83,16 +80,6 @@ public class SecurityConfiguration {
     authenticationManagerBuilder.authenticationProvider(authenticationProvider);
 
     return authenticationManagerBuilder.build();
-  }
-
-  @Bean
-  public WebMvcConfigurer corsConfigurer() {
-    return new WebMvcConfigurer() {
-      @Override
-      public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("*");
-      }
-    };
   }
 
   public void webSecurityCustomizer(WebSecurity web) {
---
+++ src/main/java/com/umulam/fleen/health/filter/SimpleCorsFilter.java
@@ -0,0 +1,37 @@
+package com.umulam.fleen.health.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCorsFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+    HttpServletResponse response = (HttpServletResponse) res;
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Credentials", "false");
+    response.setHeader("Access-Control-Allow-Methods", "*");
+    response.setHeader("Access-Control-Allow-Headers", "*");
+
+    chain.doFilter(req, response);
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) {
+  }
+
+  @Override
+  public void destroy() {
+  }
+
+}
