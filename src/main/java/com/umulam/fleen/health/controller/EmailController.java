29f4cf4b9165859ba38a7877c65c43b222e9117b
Yusuf Alamu Musa <volunux@gmail.com>
2023-08-31 23:23:53 +0100

    clean up email controller

--- src/main/java/com/umulam/fleen/health/controller/EmailController.java
+++ src/main/java/com/umulam/fleen/health/controller/EmailController.java
@@ -9,41 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "mail")
 public class EmailController {
 
-/*  public final EmailServiceImpl emailService;
-  private final DirectoryService directoryService;
-  private final GmailService gmailService;
 
-  public EmailController(EmailServiceImpl emailService,
-                         DirectoryService directoryService,
-                         GmailService gmailService) {
-    this.emailService = emailService;
-    this.directoryService = directoryService;
-    this.gmailService = gmailService;
-  }
-
-  @GetMapping
-  public boolean testEmail() {
-    EmailDetails details = EmailDetails.builder()
-            .from("volunux@gmail.com")
-            .to("volunux@gmail.com")
-            .subject("Hello World")
-            .body("Hello World Yusuf Alamu Musa")
-            .build();
-    return emailService.sendMessage(details);
-  }
-
-  @GetMapping(value = "/user")
-  public Object user() {
-    return directoryService.createUser("volunux@gmail.com");
-  }
-
-  @GetMapping(value = "/alias")
-  public Object alias() {
-    return directoryService.createEmailAlias("yusebobo@volunux.com", "Yusuf", "Musa");
-  }
-
-  @GetMapping(value = "/forward")
-  public Object forward() {
-    return gmailService.enableEmailForwarding("umulam@volunux.com", "volunux@gmail.com");
-  }*/
 }
