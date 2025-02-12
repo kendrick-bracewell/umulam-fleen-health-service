2ff3f892c48c72e5ecd086fcaa741a483cb6d5ac
Kendrick Bracewell <kendrickbracewell@gmail.com>
2025-02-12 09:07:21 +0100

    Set up feature flags for beta testing new features

--- src/main/java/com/umulam/fleen/health/util/DateFormatUtil.java
+++ src/main/java/com/umulam/fleen/health/util/DateFormatUtil.java
@@ -9,3 +9,4 @@ public class DateFormatUtil {
   public static final String DATE_TIME_NO_SECONDS = "yyyy-MM-dd'T'HH:mm";
   public static final String DATE_TIME_NO_PT_SECONDS = "yyyy-MM-dd HH:mm";
 }
+ 
